ee9dece693defa5bd2e2e0483afbfca3eb2c4909
Sergy <9058591133@mail.ru>
2023-07-05 20:30:56 +0500

    Initial commit

--- Seminar_1/task_6.py
+++ Seminar_1/task_6.py
@@ -7,6 +7,7 @@
 from random import randint
 
 LOWER_LIMIT = 0
+<<<<<<< HEAD
 UPPER_LIMIT = 1001
 
 COUNT_TRY = 10
@@ -27,3 +28,21 @@ for _ in range(COUNT_TRY + 1):
         is_win = True
 if not is_win:
     print('К сожалению вы проиграли')
+=======
+UPPER_LIMIT = 1000
+COUNT_TRY = 10
+
+RAND_NUMBER = randint(LOWER_LIMIT, UPPER_LIMIT)
+print(RAND_NUMBER)
+for i in range(COUNT_TRY):
+    number = int(input(f'Увас {COUNT_TRY - i} попыток, Введи число от 0 до 1000: '))
+    if number == RAND_NUMBER:
+        print(f'Ва угодали c {i + 1} попыток. Вы молодец!')
+        break
+    elif number < RAND_NUMBER:
+        print(f'Ваше число {number} МЕНЬШЕ загаданного')
+    else:
+        print(f'Ваше число {number} БОЛЬШЕ загаданного')
+else:
+    print('К сожалению вы проиграли')
+>>>>>>> 9ffc203 (Initial commit)
