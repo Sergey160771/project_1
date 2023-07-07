f07cc42eeb482bf1b05845151fab64427932fa78
Sergy <9058591133@mail.ru>
2023-07-07 13:19:22 +0500

    Initial commit

--- Seminar_1/task_6.py
+++ Seminar_1/task_6.py
@@ -7,28 +7,6 @@
 from random import randint
 
 LOWER_LIMIT = 0
-<<<<<<< HEAD
-UPPER_LIMIT = 1001
-
-COUNT_TRY = 10
-num = randint(LOWER_LIMIT, UPPER_LIMIT)
-RAND_NUMBER = randint(LOWER_LIMIT, UPPER_LIMIT)
-print(RAND_NUMBER)
-is_win = True
-for _ in range(COUNT_TRY + 1):
-    number = int(input('Введи число от 0 до 1000: '))
-    if number > RAND_NUMBER:
-        print('Ваше число БОЛЬШЕ загаданного')
-        is_win = False
-    elif number < RAND_NUMBER:
-        print('Ваше число МЕНЬШЕ загаданного')
-        is_win = False
-    else:
-        print('Вы выиграли!')
-        is_win = True
-if not is_win:
-    print('К сожалению вы проиграли')
-=======
 UPPER_LIMIT = 1000
 COUNT_TRY = 10
 
@@ -45,4 +23,3 @@ for i in range(COUNT_TRY):
         print(f'Ваше число {number} БОЛЬШЕ загаданного')
 else:
     print('К сожалению вы проиграли')
->>>>>>> 9ffc203 (Initial commit)
