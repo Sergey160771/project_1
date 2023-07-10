7d91c0583c6dfe87d509de92472e211b61ad4369
Sergy <9058591133@mail.ru>
2023-07-10 09:35:38 +0500

    Homework_2

--- Homework_2/task_2.py
+++ Homework_2/task_2.py
@@ -1,24 +1,28 @@
-# Напишите программу, которая принимает две строки
-# вида “a/b” — дробь с числителем и знаменателем.
-# Программа должна возвращать сумму
-# и *произведение дробей. Для проверки своего
-# кода используйте модуль fractions.
-
 import fractions
+import math
 
 number: str = input(f'Введите две дроби вида a/b через пробел: ')
 fraction_1, fraction_2 = number.split()
 numerator_1, denominator_1 = map(int, fraction_1.split('/'))
 numerator_2, denominator_2 = map(int, fraction_2.split('/'))
+sum_fractions_numerator = ((numerator_1 * denominator_2) + (numerator_2 * denominator_1))
+sum_fractions_denominator = (denominator_1 * denominator_2)
 
-sum_fractions: str = f'{(numerator_1 * denominator_2) + (numerator_2 * denominator_1)}/' \
-                f'{denominator_1 * denominator_2}'
-product_fractions: str = f'{(numerator_1 * numerator_2)}/{denominator_1 * denominator_2}'
-print(fractions.Fraction(sum_fractions), fractions.Fraction(product_fractions))
+nod_sum = math.gcd(sum_fractions_numerator,
+                   sum_fractions_denominator)
+sum_fractions_numerator = sum_fractions_numerator // nod_sum
+sum_fractions_denominator = sum_fractions_denominator // nod_sum
+product_fractions_numerator = numerator_1 * numerator_2
+product_fractions_denominator = denominator_1 * denominator_2
+
+nod_prod = math.gcd(product_fractions_numerator, product_fractions_denominator)
+product_fractions_numerator = product_fractions_numerator // nod_prod
+product_fractions_denominator = product_fractions_denominator // nod_prod
+
+print(f'{sum_fractions_numerator}/{sum_fractions_denominator} '
+      f'{product_fractions_numerator}/{product_fractions_denominator}')
 
 print(fractions.Fraction(fraction_1) + fractions.Fraction(fraction_2),
       fractions.Fraction(fraction_1) * fractions.Fraction(fraction_2))
-
-
 
 
