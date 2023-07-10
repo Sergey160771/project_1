53ae860e5c6f99b19547da08e5d76552a6f7b550
Sergy <9058591133@mail.ru>
2023-07-10 09:43:29 +0500

    Homework_2

--- Homework_2/task_2.py
+++ Homework_2/task_2.py
@@ -5,24 +5,22 @@ number: str = input(f'Введите две дроби вида a/b через �
 fraction_1, fraction_2 = number.split()
 numerator_1, denominator_1 = map(int, fraction_1.split('/'))
 numerator_2, denominator_2 = map(int, fraction_2.split('/'))
-sum_fractions_numerator = ((numerator_1 * denominator_2) + (numerator_2 * denominator_1))
-sum_fractions_denominator = (denominator_1 * denominator_2)
+sum_fractions_numerator: int = ((numerator_1 * denominator_2) + (numerator_2 * denominator_1))
+sum_fractions_denominator: int = (denominator_1 * denominator_2)
 
-nod_sum = math.gcd(sum_fractions_numerator,
-                   sum_fractions_denominator)
-sum_fractions_numerator = sum_fractions_numerator // nod_sum
-sum_fractions_denominator = sum_fractions_denominator // nod_sum
-product_fractions_numerator = numerator_1 * numerator_2
-product_fractions_denominator = denominator_1 * denominator_2
+nod_sum: int = math.gcd(sum_fractions_numerator,
+                        sum_fractions_denominator)
+sum_fractions_numerator: int = sum_fractions_numerator // nod_sum
+sum_fractions_denominator: int = sum_fractions_denominator // nod_sum
+product_fractions_numerator: int = numerator_1 * numerator_2
+product_fractions_denominator: int = denominator_1 * denominator_2
 
-nod_prod = math.gcd(product_fractions_numerator, product_fractions_denominator)
-product_fractions_numerator = product_fractions_numerator // nod_prod
-product_fractions_denominator = product_fractions_denominator // nod_prod
+nod_prod: int = math.gcd(product_fractions_numerator, product_fractions_denominator)
+product_fractions_numerator: int = product_fractions_numerator // nod_prod
+product_fractions_denominator: int = product_fractions_denominator // nod_prod
 
 print(f'{sum_fractions_numerator}/{sum_fractions_denominator} '
       f'{product_fractions_numerator}/{product_fractions_denominator}')
 
 print(fractions.Fraction(fraction_1) + fractions.Fraction(fraction_2),
       fractions.Fraction(fraction_1) * fractions.Fraction(fraction_2))
-
-
